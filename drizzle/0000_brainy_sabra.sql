b26eb0db6cc204154412f779505a8cce0bf05a83
kuroma965 <kuroma965@gmail.com>
2025-11-29 12:20:45 +0700

    clear

--- drizzle/0000_brainy_sabra.sql
+++ drizzle/0000_brainy_sabra.sql
@@ -5,6 +5,7 @@ CREATE TABLE "users" (
 	"name" varchar(100),
 	"role" varchar(50) DEFAULT 'user' NOT NULL,
 	"is_active" boolean DEFAULT true NOT NULL,
+	"sign_up" varchar(50) DEFAULT 'qooldab' NOT NULL,
 	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
 	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
 );
--- src/db/schema.js
+++ src/db/schema.js
@@ -15,6 +15,7 @@ export const users = pgTable('users', {
   name: varchar('name', { length: 100 }),
   role: varchar('role', { length: 50 }).notNull().default('user'),
   is_active: boolean('is_active').notNull().default(true),
+  sign_up: varchar('sign_up', { length: 50 }).notNull().default('qooldab'),
   created_at: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
   updated_at: timestamp('updated_at', { withTimezone: true }).notNull().defaultNow(),
 });
